3dbc6bfe10b81b181c2882874568d2ba4e4a430c
Valkinaz <valkinaz666@gmail.com>
2024-05-17 20:24:57 +0300

    Learn how to save data, reasons list, subscribe on event

--- APCounter.lua
+++ APCounter.lua
@@ -2,61 +2,93 @@ APCounter = {
     addonName = "APCounter",
     command = "/apc",
     window = APCounter_Window,
-
+    reasons = {
+        -- [40] Repair something
+        [CURRENCY_CHANGE_REASON_KEEP_REPAIR] = true,
+        -- [13] Killing a player
+        [CURRENCY_CHANGE_REASON_KILL] = true,
+        -- [74] Capture a resource (+), keep (?), outpost (?), imperial city flag (?)
+        [CURRENCY_CHANGE_REASON_OFFENSIVE_KEEP_REWARD] = true,
+        -- ?? battleground match
+        -- ?? daily reward
+    },
     vars = {},
 }
 
 function APCounter.ToggleWindow()
-    local self = APCounter
-
-    SCENE_MANAGER:ToggleTopLevel(self.window)
-end
-
-function APCounter.Log()
-    local self = APCounter
-
-    local timestamp = GetTimeStamp()
-    local hour = math.floor(timestamp / 3600)
-    local type = "kill"
-    local points = math.random(1, 1000)
-
-    if (not self.vars.journal[hour]) then self.vars.journal[hour] = {} end
-
-    table.insert(self.vars.journal[hour], {
-        timestamp,
-        type,
-        points,
-    })
+    SCENE_MANAGER:ToggleTopLevel(APCounter.window)
 end
 
 function APCounter.Get()
-    local self = APCounter
+    local timestamp = GetTimeStamp()
+    local hour = math.floor(timestamp / 3600)
+
+    if (not APCounter.vars.journal[hour]) then
+        APCounter.vars.journal[hour] = {
+            general = {},
+            repair = {},
+        }
+    end
+
+    table.insert(APCounter.vars.journal[hour]['general'], {
+        timestamp,
+        40,
+        1000,
+    })
 
     --d(self.journal)
-    d(self.vars.journal)
+    --d(self.vars.journal)
 end
 
 function APCounter.Initialize()
-    local self = APCounter
-
-    SLASH_COMMANDS[self.command] = self.ToggleWindow
-    SLASH_COMMANDS["/apc_add"] = self.Log
-    SLASH_COMMANDS["/apc_get"] = self.Get
+    SLASH_COMMANDS[APCounter.command] = APCounter.ToggleWindow
+    SLASH_COMMANDS["/apc_get"] = APCounter.Get
 
     local defaults = {
         journal = {}
     }
-    self.vars = ZO_SavedVars:NewCharacterIdSettings("APCounterSavedVariables", 1, nil, defaults)
+    APCounter.vars = ZO_SavedVars:NewCharacterIdSettings("APCounterSavedVariables", 1, nil, defaults)
 
-    SCENE_MANAGER:RegisterTopLevel(self.window, locksUIMode)
+    SCENE_MANAGER:RegisterTopLevel(APCounter.window, locksUIMode)
+    EVENT_MANAGER:RegisterForEvent(APCounter.addonName, EVENT_ALLIANCE_POINT_UPDATE, APCounter.OnAlliancePointsUpdate)
+end
+
+function APCounter.OnAlliancePointsUpdate(_, _, _, difference, reason)
+    if not APCounter.reasons[reason] then
+        return
+    end
+
+    local timestamp = GetTimeStamp()
+    local hour = math.floor(timestamp / 3600)
+
+    if (not APCounter.vars.journal[hour]) then
+        APCounter.vars.journal[hour] = {
+            -- journal for solid reasons
+            general = {},
+            -- journal for sum of repairs, prevent to spam log
+            repair = {},
+        }
+    end
+
+
+    --[[
+    TODO Для ремонта обновляем счетчик так, чтобы была сумма полученных AP за целый час. Ключ repair.
+    TODO Для всего остального пишем лог. Ключ general. В перспективе учимся расширять остальные причины деталями:
+    TODO - убийство игрока - @tag + character
+    TODO - захват ресурса/крепости/аванпоста/флага - Name of it
+    TODO -
+
+    table.insert(APCounter.vars.journal[hour], {
+        timestamp,
+        reason,
+        difference,
+    })]]
 end
 
 function APCounter.OnAddOnLoaded(event, addonName)
     if addonName ~= APCounter.addonName then return end
 
-    local self = APCounter
-
-    self.Initialize()
+    APCounter.Initialize()
     EVENT_MANAGER:UnregisterForEvent(APCounter.addonName, EVENT_ADD_ON_LOADED)
 end
 
